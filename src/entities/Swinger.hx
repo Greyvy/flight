d79b8d886dbe452d8c1660a75abb67b5efd2022b
greyvy <greyvy@gmail.com>
2018-03-29 12:19:52 -0700

    fixed file tracking so /bin is clean

--- src/entities/Swinger.hx
+++ src/entities/Swinger.hx
@@ -51,45 +51,28 @@ class Swinger extends Visual {
 
         }
 
-        var p0 = new Vec(1, 1);
-        var p1 = new Vec(30, 30);
-        // var p1 = new Vec(Math.cos(tau / 4), Math.sin(tau / 4));
-        // p1.length = 30;
+        function line() {
+            var p0 = new Vec(1, 1);
+            var p1 = new Vec(30, 30);
+            // js.Lib.debug();
 
-        // var s0 = new Vec(-thickness / 2, p0.y);
-        // var s1 = new Vec( thickness / 2, p0.y);
+            var v0 = new Vec( p0.y, -p0.x);
+            var v1 = new Vec(-p0.y,  p0.x);
+            var v2 = new Vec( p1.y, -p1.x);
+            var v3 = new Vec(-p1.y,  p1.x);
 
-        var v0 = new Vec(p0.y, -p0.x);
-        var v1 = new Vec(-p0.y, p0.x);
-        var v2 = new Vec(p1.y, -p1.x);
-        var v3 = new Vec(-p1.y, p1.x);
+            geometry.add(new Vertex(v0));
+            geometry.add(new Vertex(v1));
+            geometry.add(new Vertex(v2));
 
-        // v0.length = thickness / 2;
-        // v1.length = thickness / 2;
-        // v2.length = thickness / 2;
-        // v3.length = thickness / 2;
+            geometry.add(new Vertex(v2));
+            geometry.add(new Vertex(v3));
+            geometry.add(new Vertex(v1));
 
-        // s0.angle2D = p0.angle2D - (Math.PI / 4);
-        // s1.angle2D = p0.angle2D + (Math.PI / 4);
+            // js.Lib.debug();
+        }
 
-        // var p0_angle = p0.angle2D;
-
-        // js.Lib.debug();
-
-        geometry.add(new Vertex(v0));
-        geometry.add(new Vertex(v1));
-        geometry.add(new Vertex(v2));
-
-        geometry.add(new Vertex(v2));
-        geometry.add(new Vertex(v3));
-        geometry.add(new Vertex(v1));
-
-        // geometry.add(new Vertex(new Vec(s0.x, p1.y)));
-        // geometry.add(new Vertex(new Vec(s1.x, p0.y)));
-
-        // geometry.add(new Vertex(new Vec(s0.x, p1.y)));
-        // geometry.add(new Vertex(new Vec(s1.x, p1.y)));
-        // geometry.add(new Vertex(new Vec(s1.x, p0.y)));
+        line();
 
 
         color = new Color(1, 1, 1, 1);
